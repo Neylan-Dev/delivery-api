b71710fc33d4e03b3f3e9108ad779f70a10b9416
Neylan.Santos <neylan.santos@radixeng.com.br>
2022-07-11 11:27:14 -0300

    adição de novos metodos no controller

--- src/main/java/com/algaworks/algalog/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/controller/ClientController.java
@@ -1,30 +1,54 @@
 package com.algaworks.algalog.controller;
 
 import com.algaworks.algalog.domain.model.Client;
-import com.algaworks.algalog.repository.ClientRepository;
+import com.algaworks.algalog.domain.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/clients")
 @RequiredArgsConstructor
 public class ClientController {
 
     private final ClientRepository clientRepository;
 
-    @GetMapping("/clients")
-    public List<Client> listClients() {
+    @GetMapping
+    public List<Client> listAll() {
         return clientRepository.findAll();
     }
 
-    @PostMapping("/clients")
-    public Client createClient(@RequestBody Client client){
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> findById(@PathVariable Long clientId) {
+        return clientRepository.findById(clientId).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client client) {
         return clientRepository.save(client);
+    }
+
+    @PutMapping
+    public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client) {
+        var optionalClient = clientRepository.findById(clientId);
+        if (optionalClient.isPresent()){
+            var client1 = optionalClient.get();
+            client1.setTelephone(client.getTelephone());
+            client1.setEmail(client.getEmail());
+            return ResponseEntity.ok().body(clientRepository.save(client1));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/repository/ClientRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/ClientRepository.java
@@ -1,7 +1,13 @@
-package com.algaworks.algalog.repository;
+package com.algaworks.algalog.domain.repository;
 
 import com.algaworks.algalog.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findByName(String name);
 }
