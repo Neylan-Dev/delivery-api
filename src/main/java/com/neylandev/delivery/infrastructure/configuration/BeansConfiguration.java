42c3361583ee4529eaaec3be15ad03a0714d3fc7
Neylan-Body <neylan2011@gmail.com>
2024-08-07 22:13:37 -0300

    Ajustando security

--- src/main/java/com/neylandev/delivery/infrastructure/configuration/BeansConfiguration.java
+++ src/main/java/com/neylandev/delivery/infrastructure/configuration/BeansConfiguration.java
@@ -1,19 +1,8 @@
 package com.neylandev.delivery.infrastructure.configuration;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class BeansConfiguration {
@@ -21,26 +10,5 @@ public class BeansConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(withDefaults())
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource));
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(@Value("${cors.origins}") List<String> corsOrigins) {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowedOrigins(corsOrigins);
-        configuration.setAllowedMethods(List.of("GET"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
